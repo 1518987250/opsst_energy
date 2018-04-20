64ca7d77af729013e37fbb5f6f09a2dd50ae3065
1518987250 <1518987250@qq.com>
2018-04-20 15:55:26 +0800

    20180420下午

--- test_diff_policy_compare.m
+++ test_diff_policy_compare.m
@@ -1,13 +1,13 @@
 function [  ] = test_diff_policy_compare()
 
-M=20;%M
+M=5;%M
 Dm=10;%s
 t=0.9;%s
 T=1;%s
-c=10*10^2;%bit
+c=50*10^2;%bit
 Ep=1*10^(-8);
-for i=1:1:M
-    diff_policy_compare_Rician(i,Dm,t,T,c,Ep,'M');
+for i=100:100:c
+    diff_policy_compare_Rician(M,Dm,t,T,i,Ep,'c');
 end
 
 end
