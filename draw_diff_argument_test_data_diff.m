4699a6955bf309e36d58fa095b1506c1b19d795a
1518987250 <1518987250@qq.com>
2018-04-20 12:22:39 +0800

    20180420

--- draw_diff_argument_test_data_diff.m
+++ draw_diff_argument_test_data_diff.m
@@ -12,7 +12,7 @@ function [ ]=draw_diff_argument_test_data_diff()
            fgets(fi)%读出参数说明
            result_number=13;%12个结果
            result_value=fscanf(fi,'%d%d%f%f%f%d%d%d%d%f%f%d%f',[1,result_number]);%读取13个结果
-           EnEf(i,j)=EnEf(i,j)+result_value(5);%单位时间的平均能耗
+           EnEf(i,j)=EnEf(i,j)+result_value(3)/result_value(1);%单位时间的平均能耗
            fgets(fi)%读出回车
        end        
     end
@@ -28,13 +28,13 @@ function [ ]=draw_diff_argument_test_data_diff()
     figure();
     hold on;
     
-    plot(x,y1);
-    set(gca,'position',[0.15 0.2 0.7 0.7]);
-    xlabel('最大应用数目M');
-    ylabel('侦测效率','fontsize',20);
-    set(gca,'fontsize',20);
-    legend('OTSSP');
-    set(get(gca,'Children'),'LineWidth',2);
+%     plot(x,y1);
+%     set(gca,'position',[0.15 0.2 0.7 0.7]);
+%     xlabel('速率c(×10^{2}bps)');
+%     ylabel('侦测效率','fontsize',20);
+%     set(gca,'fontsize',20);
+%     legend('OTSSP');
+%     set(get(gca,'Children'),'LineWidth',2);
      
 %     [AX]=plotyy(x,y1,x,y4,'plot');
 %     set(gca,'position',[0.15 0.2 0.7 0.7]);
@@ -46,14 +46,14 @@ function [ ]=draw_diff_argument_test_data_diff()
 %     set(findobj(get(gca,'Children'),'LineWidth',0.5),'LineWidth',2);
 %     set(findobj(get(AX(2),'Children'),'LineWidth',0.5),'LineWidth',2);
 
-%     [AX]=plotyy(x,[y1;y4],x,y2,'plot');
-%     set(gca,'position',[0.15 0.2 0.7 0.7]);
-%     xlabel('最大应用数目M');
-%     ylabel('能耗效率(J/bit)','fontsize',22,'color','k');
-%     set(gca,'fontsize',22);
-%     legend('OTSSP','RTS','TSTB');
-%     set(AX(2),'Fontsize',22);
-%     set(findobj(get(gca,'Children'),'LineWidth',0.5),'LineWidth',2);
-%     set(findobj(get(AX(2),'Children'),'LineWidth',0.5),'LineWidth',2);
+    [AX]=plotyy(x,[y1;y4],x,y2,'plot');
+    set(gca,'position',[0.15 0.2 0.7 0.7]);
+    xlabel('最大应用数目M');
+    ylabel('能耗效率','fontsize',22,'color','k');
+    set(gca,'fontsize',22);
+    legend('OTSSP','RTS','TSTB');
+    set(AX(2),'Fontsize',22);
+    set(findobj(get(gca,'Children'),'LineWidth',0.5),'LineWidth',2);
+    set(findobj(get(AX(2),'Children'),'LineWidth',0.5),'LineWidth',2);
  end
  
--- test_diff_policy_compare.m
+++ test_diff_policy_compare.m
@@ -1,13 +1,13 @@
 function [  ] = test_diff_policy_compare()
 
-M=5;%M
-Dm=50;%s
+M=20;%M
+Dm=10;%s
 t=0.9;%s
 T=1;%s
 c=10*10^2;%bit
 Ep=1*10^(-8);
-for i=1:1:Dm
-    diff_policy_compare_Rayleigh(M,i,t,T,c,Ep,'Dm');
+for i=1:1:M
+    diff_policy_compare_Rician(i,Dm,t,T,c,Ep,'M');
 end
 
 end
