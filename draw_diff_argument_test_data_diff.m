3157d76a19b266f0797b3f5d7dbed06041e43b0c
1518987250 <1518987250@qq.com>
2018-04-22 21:33:48 +0800

    20180422

--- draw_diff_argument_test_data_diff.m
+++ draw_diff_argument_test_data_diff.m
@@ -1,8 +1,8 @@
 function [ ]=draw_diff_argument_test_data_diff()
    
 	case_number=5;%5种策略比较
-    Dm_number=20;%100种速率
-    fi=fopen('test_compare_diff_M_Rician.txt','r');
+    Dm_number=50;%100种速率
+    fi=fopen('test_compare_diff_c_Rician1.txt','r');
     for i=1:Dm_number
        fgets(fi)%读出参数说明
        fgets(fi);%读出回车
@@ -12,12 +12,12 @@ function [ ]=draw_diff_argument_test_data_diff()
            fgets(fi)%读出参数说明
            result_number=13;%12个结果
            result_value=fscanf(fi,'%d%d%f%f%f%d%d%d%d%f%f%d%f',[1,result_number]);%读取13个结果
-           EnEf(i,j)=EnEf(i,j)+result_value(3)/result_value(2);%单位时间的平均能耗
+           EnEf(i,j)=EnEf(i,j)+result_value(3)/result_value(1);%单位时间的平均能耗
            fgets(fi)%读出回车
        end        
     end
     
-    x=[1:1:20];
+    x=[1:1:50];
     for i=1:Dm_number
         y1(i)=EnEf(i,1);
         y2(i)=EnEf(i,2);
@@ -29,9 +29,10 @@ function [ ]=draw_diff_argument_test_data_diff()
     hold on;
  
 %     plot(x,y1);
+% %     axis([0,10,0.05,0.3])
 %     set(gca,'position',[0.15 0.25 0.7 0.7]);
-%     xlabel('速率c(×10^{2}bps)');
-%     ylabel('平均能耗(J/s)','fontsize',20);
+%     xlabel('最大传输延时Dm(s)');
+%     ylabel('侦测效率','fontsize',20);
 %     set(gca,'fontsize',20);
 %     legend('OTSSP');
 %     set(get(gca,'Children'),'LineWidth',2);
@@ -47,9 +48,9 @@ function [ ]=draw_diff_argument_test_data_diff()
 %     set(findobj(get(AX(2),'Children'),'LineWidth',0.5),'LineWidth',2);
 
     [AX]=plotyy(x,[y1;y4],x,y2,'plot');
-    set(gca,'position',[0.15 0.25 0.7 0.7]);
-    xlabel('最大应用数目M');
-    ylabel('能耗效率{\elta}','fontsize',22,'color','k');
+    set(gca,'position',[0.15 0.25 0.7 0.63]);
+    xlabel('速率c(×10^{2}bps)');
+    ylabel('能耗效率','fontsize',22,'color','k');
     set(gca,'fontsize',22);
     legend('OTSSP','RTS','TSTB');
     set(AX(2),'Fontsize',22);
--- test_diff_policy_compare.m
+++ test_diff_policy_compare.m
@@ -4,10 +4,10 @@ M=5;%M
 Dm=10;%s
 t=0.9;%s
 T=1;%s
-c=50*10^2;%bit
+c=25*10^2;%bit
 Ep=1*10^(-8);
-for i=100:100:c
-    diff_policy_compare_Rician(M,Dm,t,T,i,Ep,'c');
+for i=100:50:c
+    diff_policy_compare_Rayleigh(M,Dm,t,T,i,Ep,'c');
 end
 
 end
