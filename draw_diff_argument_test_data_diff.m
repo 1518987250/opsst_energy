9bec1f44ffae6f32ea06f4fa8a5f476d188bd47b
1518987250 <1518987250@qq.com>
2018-04-24 12:08:34 +0800

    20180424中午

--- draw_diff_argument_test_data_diff.m
+++ draw_diff_argument_test_data_diff.m
@@ -12,7 +12,7 @@ function [ ]=draw_diff_argument_test_data_diff()
            fgets(fi)%读出参数说明
            result_number=13;%12个结果
            result_value=fscanf(fi,'%d%d%f%f%f%d%d%d%d%f%f%d%f',[1,result_number]);%读取13个结果
-           EnEf(i,j)=EnEf(i,j)+result_value(3)/result_value(1);%单位时间的平均能耗
+           EnEf(i,j)=EnEf(i,j)+result_value(3)/result_value(2);%单位时间的平均能耗
            fgets(fi)%读出回车
        end        
     end
@@ -28,14 +28,14 @@ function [ ]=draw_diff_argument_test_data_diff()
     figure();
     hold on;
  
-%     plot(x,y1);
-% %     axis([0,10,0.05,0.3])
-%     set(gca,'position',[0.15 0.25 0.7 0.7]);
-%     xlabel('最大传输延时Dm(s)');
-%     ylabel('侦测效率','fontsize',20);
-%     set(gca,'fontsize',20);
-%     legend('OTSSP');
-%     set(get(gca,'Children'),'LineWidth',2);
+    plot(x,y1);
+%     axis([0,10,0.05,0.3])
+    set(gca,'position',[0.15 0.25 0.7 0.7]);
+    xlabel('速率c(×10^{2}bps)');
+    ylabel('侦测效率','fontsize',20);
+    set(gca,'fontsize',20);
+    legend('OTSSP');
+    set(get(gca,'Children'),'LineWidth',2);
      
 %     [AX]=plotyy(x,y2,x,y4,'plot');
 %     set(gca,'position',[0.15 0.25 0.7 0.7]);
@@ -47,14 +47,14 @@ function [ ]=draw_diff_argument_test_data_diff()
 %     set(findobj(get(gca,'Children'),'LineWidth',0.5),'LineWidth',2);
 %     set(findobj(get(AX(2),'Children'),'LineWidth',0.5),'LineWidth',2);
 
-    [AX]=plotyy(x,[y1;y4],x,y2,'plot');
-    set(gca,'position',[0.15 0.25 0.7 0.63]);
-    xlabel('速率c(×10^{2}bps)');
-    ylabel('能耗效率','fontsize',22,'color','k');
-    set(gca,'fontsize',22);
-    legend('OTSSP','RTS','TSTB');
-    set(AX(2),'Fontsize',22);
-    set(findobj(get(gca,'Children'),'LineWidth',0.5),'LineWidth',2);
-    set(findobj(get(AX(2),'Children'),'LineWidth',0.5),'LineWidth',2);
+%     [AX]=plotyy(x,[y1;y4],x,y2,'plot');
+%     set(gca,'position',[0.15 0.25 0.7 0.63]);
+%     xlabel('速率c(×10^{2}bps)');
+%     ylabel('能耗效率','fontsize',22,'color','k');
+%     set(gca,'fontsize',22);
+%     legend('OTSSP','RTS','TSTB');
+%     set(AX(2),'Fontsize',22);
+%     set(findobj(get(gca,'Children'),'LineWidth',0.5),'LineWidth',2);
+%     set(findobj(get(AX(2),'Children'),'LineWidth',0.5),'LineWidth',2);
  end
  
