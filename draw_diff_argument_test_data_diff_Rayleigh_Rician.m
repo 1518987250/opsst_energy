25aa53ce078ad2a8b7cae522ebaa917b291b815d
1518987250 <1518987250@qq.com>
2018-07-05 15:35:24 +0800

    20180705

--- draw_diff_argument_test_data_diff_Rayleigh_Rician.m
+++ draw_diff_argument_test_data_diff_Rayleigh_Rician.m
@@ -1,7 +1,7 @@
 function [ ]=draw_diff_argument_test_data_diff_Rayleigh_Rician()
    
 	case_number=5;%5种策略比较
-    Dm_number=10;%100种速率
+    Dm_number=15;%100种速率
     fi=fopen('test_compare_diff_c_Rayleigh.txt','r');
     for i=1:Dm_number
        fgets(fi)%读出参数说明
@@ -12,7 +12,7 @@ function [ ]=draw_diff_argument_test_data_diff_Rayleigh_Rician()
            fgets(fi)%读出参数说明
            result_number=13;%12个结果
            result_value=fscanf(fi,'%d%d%f%f%f%d%d%d%d%f%f%d%f',[1,result_number]);%读取13个结果
-           EnEf_rayleigh(i,j)=EnEf_rayleigh(i,j)+result_value(3)/result_value(1);%单位时间的平均能耗
+           EnEf_rayleigh(i,j)=EnEf_rayleigh(i,j)+result_value(3)/result_value(2);%单位时间的平均能耗
            fgets(fi)%读出回车
        end 
     end
@@ -26,12 +26,12 @@ function [ ]=draw_diff_argument_test_data_diff_Rayleigh_Rician()
            fgets(fi)%读出参数说明
            result_number=13;%12个结果
            result_value=fscanf(fi,'%d%d%f%f%f%d%d%d%d%f%f%d%f',[1,result_number]);%读取13个结果
-           EnEf_rician(m,n)=EnEf_rician(m,n)+result_value(3)/result_value(1);%单位时间的平均能耗
+           EnEf_rician(m,n)=EnEf_rician(m,n)+result_value(3)/result_value(2);%单位时间的平均能耗
            fgets(fi)%读出回车
        end        
     end
     
-    x=[1:1:10];
+    x=[1:1:15];
     for i=1:Dm_number
         y1(i)=EnEf_rayleigh(i,1);
         y2(i)=EnEf_rayleigh(i,2);
@@ -56,20 +56,23 @@ function [ ]=draw_diff_argument_test_data_diff_Rayleigh_Rician()
 %     set(get(gca,'Children'),'LineWidth',2);
 %     box on;
     
-    plot(x,y1);
-    plot(x,y2);
-    plot(x,y4);
-    plot(x,y6);
-    plot(x,y7);
-    plot(x,y9);
-    set(gca,'position',[0.15 0.23 0.7 0.7]);
-    xlabel('最大应用数目M');
-    ylabel('平均能耗(J/bit)','fontsize',20);
-    set(gca,'fontsize',20);
-    legend('Rayleigh1','Rayleigh2','Rayleigh3','Rician4','Rician5','Rician6');
-    set(get(gca,'Children'),'LineWidth',2);
-    box on;
-     
+%     plot(x,y1);
+%     plot(x,y2);
+%     plot(x,y4);
+%     plot(x,y6);
+%     plot(x,y7);
+%     plot(x,y9);
+   
+%     subplot(1,2,1);plot(x,y1,x,y2,x,y4);xlabel('速率c(×10^{2}bps)');ylabel('能耗效率','fontsize',20);legend('OTSSP','TSTB','RTS');
+%     set(gca,'position',[0.15 0.23 0.7 0.7]);
+%     xlabel('最大应用数目M');
+%     ylabel('平均能耗(J/bit)','fontsize',20);
+%     set(gca,'fontsize',20);
+%     legend('Rayleigh1','Rayleigh2','Rayleigh3','Rician4','Rician5','Rician6');
+%     set(get(gca,'Children'),'LineWidth',2);
+%     subplot(1,2,2);plot(x,y6,x,y7,x,y9);xlabel('速率c(×10^{2}bps)');legend('OTSSP','TSTB','RTS'); 
+
+    
 %     [AX]=plotyy(x,y2,x,y4,'plot');
 %     set(gca,'position',[0.15 0.25 0.7 0.7]);
 %     xlabel('速率c(×10^{2}bps)');
@@ -80,15 +83,18 @@ function [ ]=draw_diff_argument_test_data_diff_Rayleigh_Rician()
 %     set(findobj(get(gca,'Children'),'LineWidth',0.5),'LineWidth',2);
 %     set(findobj(get(AX(2),'Children'),'LineWidth',0.5),'LineWidth',2);
 
-%     [AX]=plotyy(x,[y1;y4],x,y2,'plot');
-%     set(gca,'position',[0.15 0.25 0.7 0.63]);
-%     xlabel('最大应用数目M');
-%     ylabel('平均能耗(J/bit)','fontsize',22,'color','k');
-%     set(gca,'fontsize',22);
-%     legend('OTSSP','RTS','TSTB');
-%     set(AX(2),'Fontsize',22);
+%     subplot(1,2,1);[AX]=plotyy(x,[y1;y4],x,y2,'plot');xlabel('速率c(×10^{2}bps)');ylabel('平均能耗(J/bit)','fontsize',22,'color','k');legend('OTSSP','RTS','TSTB');set(gca,'fontsize',22);set(AX(2),'Fontsize',22);
+%     subplot(1,2,2);[AX]=plotyy(x,[y6;y9],x,y7,'plot');xlabel('速率c(×10^{2}bps)');legend('OTSSP','RTS','TSTB');set(gca,'fontsize',22);set(AX(2),'Fontsize',22);
+%     set(gca,'position',[0.15 0.25 0.7 0.63]);    
+%     legend('OTSSP','RTS','TSTB'); 
 %     set(findobj(get(gca,'Children'),'LineWidth',0.5),'LineWidth',2);
 %     set(findobj(get(AX(2),'Children'),'LineWidth',0.5),'LineWidth',2);
-%     box on;
+
+    subplot(2,2,1);plot(x,y1);xlabel('速率c(×10^{2}bps)');ylabel('能耗效率','fontsize',22,'color','k');legend('OTSSP');
+    subplot(2,2,2);[AX]=plotyy(x,y2,x,y4,'plot');xlabel('速率c(×10^{2}bps)');legend('RTS','TSTB');set(gca,'fontsize',22);set(AX(2),'Fontsize',22);
+    subplot(2,2,3);plot(x,y6);xlabel('速率c(×10^{2}bps)');ylabel('能耗效率','fontsize',22,'color','k');legend('OTSSP');
+    subplot(2,2,4);[AX]=plotyy(x,y7,x,y9,'plot');xlabel('速率c(×10^{2}bps)');legend('RTS','TSTB');set(gca,'fontsize',22);set(AX(2),'Fontsize',22);
+    
+    box on;
  end
  
